a93df6405ab7edce13efaa2210ca11453c5f0801
Sabina Aledort <saledort@redhat.com>
2024-11-18 12:40:47 +0200

    network-flow-matrix: run specific branch based on the OpenShift version

--- ci-operator/step-registry/network-flow-matrix/tests/network-flow-matrix-tests-commands.sh
+++ ci-operator/step-registry/network-flow-matrix/tests/network-flow-matrix-tests-commands.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Extract and format the oc version to branch
+extract_version() {
+  version=$(oc version | grep "Server Version:" | awk '{print $3}')
+  major=$(echo "$version" | cut -d '.' -f 1)
+  minor=$(echo "$version" | cut -d '.' -f 2)
+  oc_branch="release-$major.$minor"
+  echo "$oc_branch"
+}
+OC_BRANCH=$(extract_version)
+
 source $HOME/golang-1.22.4
 echo "Go version: $(go version)"
 git clone https://github.com/openshift-kni/commatrix ${SHARED_DIR}/commatrix
 pushd ${SHARED_DIR}/commatrix || exit
-git checkout origin_test_branch
+git checkout ${OC_BRANCH}
 go mod vendor
 make e2e-test
 popd || exit
